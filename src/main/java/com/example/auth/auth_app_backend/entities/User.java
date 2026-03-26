92c56df556433d484d4476119419c1d9c6fb22fc
Rishabh <prof.id.rishabhnegi@gmail.com>
2026-03-26 23:56:20 +0530

    Adding User Entity

--- src/main/java/com/example/auth/auth_app_backend/entities/User.java
+++ src/main/java/com/example/auth/auth_app_backend/entities/User.java
@@ -1,11 +1,25 @@
 package com.example.auth.auth_app_backend.entities;
 
+import jakarta.persistence.*;
+import lombok.*;
+
 import java.time.Instant;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "user_id")
     private UUID id;
     private String email;
     private String name;
