7e5bc27a2e1c6205228115779d483737e45fb0a5
Rishabh <prof.id.rishabhnegi@gmail.com>
2026-03-27 23:35:52 +0530

    Add Role Entity

--- src/main/java/com/example/auth/auth_app_backend/entities/Role.java
+++ src/main/java/com/example/auth/auth_app_backend/entities/Role.java
@@ -1,9 +1,22 @@
 package com.example.auth.auth_app_backend.entities;
 
+import jakarta.persistence.*;
+import lombok.*;
+
 import java.util.UUID;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 
+@Table (name = "user_roles")
 public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id = UUID.randomUUID();
+
+    @Column(unique = true, nullable = false)
     private String name;
 }
