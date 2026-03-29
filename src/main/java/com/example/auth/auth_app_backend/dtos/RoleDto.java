2e8b316dbeef5d6309988f1e4057b9fd47f716c2
Rishabh <prof.id.rishabhnegi@gmail.com>
2026-03-29 18:20:50 +0530

    Adding DTOS

---
+++ src/main/java/com/example/auth/auth_app_backend/dtos/RoleDto.java
@@ -0,0 +1,8 @@
+package com.example.auth.auth_app_backend.dtos;
+
+import java.util.UUID;
+
+public class RoleDto {
+    private UUID id;
+    private String name;
+}
---
+++ src/main/java/com/example/auth/auth_app_backend/repositories/UserRepo.java
@@ -0,0 +1,4 @@
+package com.example.auth.auth_app_backend.repositories;
+
+public interface UserRepo {
+}
