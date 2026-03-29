a39bd0b5210ba0fbeee64fbfd939c7e95124e903
Rishabh <prof.id.rishabhnegi@gmail.com>
2026-03-29 18:53:17 +0530

    Creating UserService

--- src/main/java/com/example/auth/auth_app_backend/Services/UserService.java
+++ src/main/java/com/example/auth/auth_app_backend/Services/UserService.java
@@ -1,5 +1,17 @@
 package com.example.auth.auth_app_backend.Services;
 
-public interface UserService {
+import com.example.auth.auth_app_backend.dtos.UserDto;
 
+
+public interface UserService {
+    //Create User
+    UserDto createUser(UserDto userDto);
+    //Update User
+    UserDto updateUser(UserDto userDto, String userId);
+    //Delete User
+    void deleteUser(String id);
+
+    UserDto getUserById(String id);
+    UserDto getUserByEmail(String email);
+    Iterable<UserDto> getAllUsers();
 }
---
+++ src/main/java/com/example/auth/auth_app_backend/Services/UserServiceImpl.java
@@ -0,0 +1,4 @@
+package com.example.auth.auth_app_backend.Services;
+
+public class UserServiceImpl {
+}
