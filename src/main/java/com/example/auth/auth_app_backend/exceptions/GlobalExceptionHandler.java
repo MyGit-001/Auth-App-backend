a2319c9c07da5e90414dfd0f86f641ebf06da3aa
Rishabh <prof.id.rishabhnegi@gmail.com>
2026-04-06 22:04:12 +0530

    Adding ResourceNotFound handler

--- src/main/java/com/example/auth/auth_app_backend/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/example/auth/auth_app_backend/exceptions/GlobalExceptionHandler.java
@@ -1,14 +1,18 @@
 package com.example.auth.auth_app_backend.exceptions;
-
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.ErrorResponse;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import com.example.auth.auth_app_backend.dtos.ErrorResponse;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     //resource not found exception handler
+    @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleResourceNotFoundException(ResourceNotFoundException exception){
-
+       ErrorResponse errorResponse = new ErrorResponse(exception.getMessage() , HttpStatus.NOT_FOUND
+               , "Resource Not Found" );
+       return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
     }
 }
