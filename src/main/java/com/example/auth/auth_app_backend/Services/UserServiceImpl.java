77301d5e629c51150f8bf624844bb790217b2f20
Rishabh <prof.id.rishabhnegi@gmail.com>
2026-04-09 19:53:03 +0530

    Implementing APIs

--- src/main/java/com/example/auth/auth_app_backend/Services/UserServiceImpl.java
+++ src/main/java/com/example/auth/auth_app_backend/Services/UserServiceImpl.java
@@ -4,11 +4,14 @@ import com.example.auth.auth_app_backend.dtos.UserDto;
 import com.example.auth.auth_app_backend.entities.Provider;
 import com.example.auth.auth_app_backend.entities.User;
 import com.example.auth.auth_app_backend.exceptions.ResourceNotFoundException;
+import com.example.auth.auth_app_backend.helpers.UserHelper;
 import com.example.auth.auth_app_backend.repositories.UserRepo;
 import jakarta.transaction.Transactional;
 import jakarta.validation.constraints.Null;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 public class UserServiceImpl implements UserService{
@@ -44,11 +47,15 @@ public class UserServiceImpl implements UserService{
 
     @Override
     public void deleteUser(String id) {
+        UUID uId = UserHelper.parseUUID(id);
+         User user = userRepo.findById(uId).orElseThrow( ()-> new ResourceNotFoundException("User not found by ID") );
+        userRepo.delete(user);
     }
 
     @Override
     public UserDto getUserById(String id) {
-        return null;
+        User user = userRepo.findById(UserHelper.parseUUID(id)).orElseThrow( ()-> new ResourceNotFoundException("User Not found by ID") );
+        return modelMapper.map(user , UserDto.class);
     }
 
     @Override
